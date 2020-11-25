fba9068f54b1c65707e3ccd1f94165a4577b3b32
MaQueen <shusineko@icloud.com>
2020-11-25 09:58:55 +0900

    commit

--- resources/views/book/layout.blade.php
+++ resources/views/book/layout.blade.php
@@ -1,5 +1,6 @@
 <head>
   <title>Laravel Sample</title>
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <link rel="stylesheet" type="text/scss" href="css/style.scss">
 </head>
 @yield('content')
