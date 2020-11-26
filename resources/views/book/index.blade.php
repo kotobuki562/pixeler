c353d0ccbce554e278931b7db4a9472eeaa2ff7c
MaQueen <shusineko@icloud.com>
2020-11-26 13:55:52 +0900

    commit

--- resources/views/book/index.blade.php
+++ resources/views/book/index.blade.php
@@ -1,46 +1,50 @@
 <head>
   <title>Laravel Sample</title>
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-  <link rel='stylesheet' href='https://unpkg.com/ress/dist/ress.min.css'>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+  <link rel="stylesheet" href="css/style.css">
 </head>
+@if (Route::has('login'))
+    <div class="top-right links">
+        @auth
+            <a href="{{ url('/home') }}">Home</a>
+            <a href="{{ url('/book') }}">Book</a>
+        @else
+            <a href="{{ route('login') }}">Login</a>
+
+            @if (Route::has('register'))
+                <a href="{{ route('register') }}">Register</a>
+            @endif
+        @endauth
+    </div>
+@endif
+
 @extends('book/layout')
 @section('content')
 <div class="container ops-main">
 <div class="row">
-@extends('book/layout')
-@section('content')
   <div class="col-md-12">
     <h3 class="ops-title">書籍一覧</h3>
   </div>
 </div>
 <div class="row">
   <div class="col-md-11 col-md-offset-1">
-    <table class="table text-center">
-      <tr>
-        <th class="text-center">ID</th>
-        <th class="text-center">書籍名</th>
-        <th class="text-center">価格</th>
-        <th class="text-center">著者</th>
-        <th class="text-center">削除</th>
-      </tr>
       @foreach($books as $book)
-      <tr>
-        <td>
-          <a href="/book/{{ $book->id }}/edit">{{ $book->id }}</a>
-        </td>
-        <td>{{ $book->name }}</td>
-        <td>{{ $book->price }}</td>
-        <td>{{ $book->author }}</td>
-        <td>
+      
+      <div class="card" style="width: 18rem;">
+        <div class="card-body">
+          <h5 class="card-title">{{ $book->name }}</h5>
+          <p class="card-text">{{ $book->price }}</p>
+          <p class="card-text">{{ $book->author }}</p>
+          <a class="btn btn-primary" href="/book/{{ $book->id }}/edit">{{ $book->id }}</a>
           <form action="/book/{{ $book->id }}" method="post">
             <input type="hidden" name="_method" value="DELETE">
             <input type="hidden" name="_token" value="{{ csrf_token() }}">
             <button type="submit" class="btn btn-xs btn-danger" aria-label="Left Align"><span class="glyphicon glyphicon-trash"></span></button>
           </form>
-        </td>
-      </tr>
+        </div>
+      </div>
+
       @endforeach
-    </table>
     <div><a href="/book/create" class="btn btn-default">新規作成</a></div>
   </div>
 </div>
--- resources/views/book/message.blade.php
+++ resources/views/book/message.blade.php
@@ -2,11 +2,11 @@
     <div class="col-md-12">
     @if ($errors->any())
         <div class="alert alert-danger">
-          <ul>
-              @foreach ($errors->all() as $error)
-                  <li>{{ $error }}</li>
-              @endforeach
-          </ul>
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
         </div>
     @endif
     </div>
